337cd49beecb2b4d103e2dbeab2cc4e8f57ba0bf
Nikita <mrcoopgmz@gmail.com>
2025-12-28 14:24:06 +0300

    Забыл импорт

--- FirstCRUD/src/main/java/ru/lunathor/crud/controller/UserController.java
+++ FirstCRUD/src/main/java/ru/lunathor/crud/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import ru.lunathor.crud.model.User;
 import ru.lunathor.crud.service.UserService;
 import java.util.List;
